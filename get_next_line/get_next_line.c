ac9705392da066fb0b74dcf3af9b76d195d6e6f0
Youness Laarare <ylaarare@c2r6p4.1337.ma>
2024-11-21 21:11:08 +0100

    implemented the line return

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -1,17 +1,37 @@
 #include "get_next_line.h"
 
+char	*get_line(char *buffer, char *pos)
+{
+	char	*line;
+	char	*tmp;
+	size_t	size_of_line;
+
+	size_of_line = pos - buffer + 2;
+	line = malloc(size_of_line);
+	if (!line)
+		return (NULL);
+	tmp = line;
+	while (--size_of_line)
+		*tmp++ = *buffer++;
+	*tmp = 0;
+	return (line);
+}
 char *get_next_line(int fd)
 {
 	static char	buffer[BUFFER_SIZE];
-	size_t		size_of_line;
+	char		tmp[BUFFER_SIZE];
 	char		*new_line_pos;
+	char		*line;
 
-	if (read(fd, buffer, BUFFER_SIZE))
-		return (NULL);
-	new_line_pos = ft_strchr(buffer, '\n');
-	if (new_line_pos)
+	while (1)
 	{
-		size_of_line = new_line_pos - buffer + 1;
+		if (read(fd, buffer, BUFFER_SIZE) <= 0)
+			return (NULL);
+		new_line_pos = ft_strchr(buffer, '\n');
+		if (new_line_pos)
+		{
+			line = get_line(buffer, new_line_pos);
+			return (line);
+		}
 	}
-	return (buffer);
 }
